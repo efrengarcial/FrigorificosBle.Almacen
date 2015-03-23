61bd7cb75372129ad947ff7e8261697f030e2e9c
Dario <dariodeath@gmail.com>
2015-03-23 16:56:28 -0500

    consulta orden

---
+++ FrigorificosBle.Almacen.Core/Domain/Model/Orden.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrigorificosBle.Almacen.Core.Domain
+{
+    public partial class Orden
+    {
+        public long FechaCreacionLong { 
+            get 
+            { 
+                return this.FechaCreacion.Ticks; 
+            } 
+        }
+    }
+}
