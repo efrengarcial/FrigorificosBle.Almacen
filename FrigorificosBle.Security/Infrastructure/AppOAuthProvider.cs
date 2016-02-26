1dbe7254cb452dda08ae70fbbba1f73ee252bf9d
efrengarcial <efren.gl@gmail.com>
2016-02-25 20:53:15 -0500

    Cambio de version de Log4net y manejo de errores

--- FrigorificosBle.Security/Infrastructure/AppOAuthProvider.cs
+++ FrigorificosBle.Security/Infrastructure/AppOAuthProvider.cs
@@ -9,11 +9,14 @@ using System.Collections.Generic;
 using FrigorificosBle.Security.Dao;
 using System.Linq;
 using System.Data.Entity;
+using log4net;
+using System.Reflection;
 
 namespace FrigorificosBle.Security.Infrastructure
 {
     public class ApplicationOAuthProvider : OAuthAuthorizationServerProvider
     {
+        private readonly ILog logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         private readonly string _publicClientId;
 
         public ApplicationOAuthProvider(string publicClientId)
@@ -30,13 +33,17 @@ namespace FrigorificosBle.Security.Infrastructure
         {
             var userManager = context.OwinContext.GetUserManager<ApplicationUserManager>();
             var rolManager = context.OwinContext.GetUserManager<ApplicationRoleManager>();
-
-
-            ApplicationUser user = await userManager.FindAsync(context.UserName, context.Password);
-            List<int> roles = new List<int>();
-            foreach (var item in  user.Roles ){
-                roles.Add(item.RoleId); 
+            ApplicationUser user = null;
+            try { 
+                user = await userManager.FindAsync(context.UserName, context.Password);
             }
+            catch (Exception ex)
+            {
+                logger.Error(ex);
+                context.SetError("error", "Error de Sistema.");
+                return;
+            }
+   
 
             if (user == null)
             {
@@ -48,6 +55,11 @@ namespace FrigorificosBle.Security.Infrastructure
             {
                 context.SetError("invalid_grant", "User did not confirm email.");
                 return;
+            }
+            List<int> roles = new List<int>();
+            foreach (var item in user.Roles)
+            {
+                roles.Add(item.RoleId);
             }
             List<ApplicationRole> rolesList = rolManager.Roles.Where(r => roles.Contains(r.Id)).
                 Include(r => r.Permissions.Select(p => p.Permission)).ToList();
